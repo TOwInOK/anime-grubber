d74a772ef7cc12f2ee042b16b392c7b188f72e36
TOwInOK 60252419+TOwInOK@users.noreply.github.com <60252419+TOwInOK@users.noreply.github.com>
2024-10-28 01:13:49 +0700

    add docstring for Waifu macros

--- src/agents/macros.rs
+++ src/agents/macros.rs
@@ -1,4 +1,23 @@
 #[macro_export]
+/// Macros for generate [Waifu]
+/// # Examples
+/// if you need default
+/// fn default() {
+///     let standard = waifu_pics!();
+///     assert_eq!(standard.categorie, Categories::SFW(waifu_pics::SFW::Waifu))
+/// }
+/// if you need to choose category with expr
+/// fn with_categories() {
+///     let expected_categorie = Categories::SFW(waifu_pics::SFW::Blush);
+///     let custom = waifu_pics!(expected_categorie);
+///     assert_eq!(custom.categorie, expected_categorie)
+/// }
+/// if you need to choose category with tokens
+/// fn with_aspects() {
+///     let expected_categorie = Categories::SFW(waifu_pics::SFW::Blush);
+///     let custom = waifu_pics!(SFW, Blush);
+///     assert_eq!(custom.categorie, expected_categorie)
+/// }
 macro_rules! waifu_pics {
     ($($categorie:expr)?) => {{
         use $crate::agents::waifu_pics::Waifu;
